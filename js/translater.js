a8ec28fc699559d9e1a3c4835e3256177e8be019
frogif <skyfrog@163.com>
2022-03-14 11:44:43 +0800

    multi-select color edit

--- js/translater.js
+++ js/translater.js
@@ -2,14 +2,16 @@ const zh = {
     "rebase": "变基",
     "reset": "重置",
     "home": "归位",
-    "run": "运行"
+    "run": "运行",
+    "color.edit.tip":"按'ctrl'多选"
 };
 
 const en = {
     "rebase": "rebase",
     "reset": "reset",
     "home": "home",
-    "run": "run"
+    "run": "run",
+    "color.edit.tip":"press 'ctrl' to multi-select"
 };
 
 export function translate(key, lang){
